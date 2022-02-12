b0953c921d9d4799509e1a52c065c8cdf6b4cbfe
kamujumanjari <99378533+kamujumanjari@users.noreply.github.com>
2022-02-12 16:37:30 +0530

    Add files via upload

---
+++ MainClass.java
@@ -0,0 +1,29 @@
+public class MainClass{
+	
+	public static void main(String[] args){
+		
+		System.out.println("=============");
+
+		
+		Shamim sh=new Shamim();
+		sh.printMsg();
+		
+
+		Kishan kishan = new Kishan();
+		Rasim rasim=new Rasim();
+		Ako a=new Ako();
+		Manjari b=new Manjari();
+		rasim.printMsg();
+
+		Danish obj = new Danish();
+		obj.printMsg();
+		kishan.printMsg();
+		a.printMsg();
+		b.printMsg();
+
+		
+		System.out.println("=============");
+		
+	}
+	
+}
