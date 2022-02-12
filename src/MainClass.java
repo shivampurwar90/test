cc43146ae3bb55be05587b525bbda5ff3a82e47d
kamujumanjari <kamujumanjari@gmail.com>
2022-02-12 18:01:11 +0530

    Manjari

--- src/MainClass.java
+++ src/MainClass.java
@@ -12,12 +12,14 @@ public class MainClass{
 		Kishan kishan = new Kishan();
 		Rasim rasim=new Rasim();
 		Ako a=new Ako();
+                Manjari b=new Manjari();
 		rasim.printMsg();
 
 		Danish obj = new Danish();
 		obj.printMsg();
 		kishan.printMsg();
 		a.printMsg();
+                b.printMsg();
 
 		
 		System.out.println("=============");
